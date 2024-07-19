775996579eb2467256076a3f0e1afb6d44032f11
Ian W <49083526+Landaman@users.noreply.github.com>
2024-07-18 21:40:11 -0500

    Adds page metadata and a package manger

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,10 +3,6 @@
 	import MenuBar from '$lib/components/menu-bar.svelte';
 </script>
 
-<svelte:head>
-	<title>Spotify Couples</title>
-</svelte:head>
-
 <div class="w-full h-full flex flex-col gap-2">
 	<MenuBar />
 	<hr class="border-border/60" />
