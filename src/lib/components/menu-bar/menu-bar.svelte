c860979a7c53428e7f73dace288c81ae3b8092cf
Ian W <49083526+Landaman@users.noreply.github.com>
2024-07-23 21:53:25 -0500

    Added website name text

--- src/lib/components/menu-bar/menu-bar.svelte
+++ src/lib/components/menu-bar/menu-bar.svelte
@@ -4,12 +4,15 @@
 
 <header class="flex w-full flex-row justify-between px-4 pt-2 md:px-8">
 	<a href="/">
-		<enhanced:img
-			src="$lib/assets/logo.png"
-			alt="Spotify Couples"
-			sizes="10px,10px"
-			class="pointer-events-none h-10 w-10"
-		/>
+		<div class="pointer-events-none flex flex-row items-center gap-2">
+			<enhanced:img
+				src="$lib/assets/logo.png"
+				alt="Spotify Couples"
+				sizes="10px,10px"
+				class=" h-10 w-10"
+			/>
+			<h1 class="hidden text-2xl font-semibold tracking-wide md:block">Spotify Couples</h1>
+		</div>
 	</a>
 
 	<UserManagementDropdown />
