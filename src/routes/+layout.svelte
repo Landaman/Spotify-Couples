af6e88b4b6e1dd82d185f88909cbc11485ec0878
Ian W <49083526+Landaman@users.noreply.github.com>
2024-07-26 21:39:29 -0500

    Added head icons and theme information

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,6 +6,8 @@
 	import { page } from '$app/stores';
 </script>
 
+<ModeWatcher themeColors={{ light: '#FFFFFF', dark: '#020817' }} />
+
 <svelte:head>
 	<title>{$page.data.pageTitle ? `${$page.data.pageTitle} | ` : ''}Spotify Couples</title>
 	<meta
@@ -15,7 +17,6 @@
 </svelte:head>
 
 <div>
-	<ModeWatcher />
 	<MenuBar class="fixed top-0" />
 	<div class="flex min-h-screen w-full flex-shrink-0 flex-grow flex-col gap-3">
 		<MenuBar class="invisible" />
