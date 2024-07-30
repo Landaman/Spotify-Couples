d2912a2b3a170d0a9aec04fd3d663f06bebfb124
Ian W <49083526+Landaman@users.noreply.github.com>
2024-07-30 18:28:34 -0500

    Include a working wave hero section

--- src/lib/components/hero-section/wave.svelte
+++ src/lib/components/hero-section/wave.svelte
@@ -1,44 +1,85 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
-	import { cubicInOut } from 'svelte/easing';
+	import type { SVGAttributes } from 'svelte/elements';
 	import { tweened } from 'svelte/motion';
 
+	type $$Props = Omit<SVGAttributes<SVGRectElement>, 'y' & 'height'> & {
+		/**
+		 * Maximum height to allow on the rectangle
+		 */
+		maxHeight: number;
+		/**
+		 * Minimum height to allow on the rectangle (defaults to 0)
+		 */
+		minHeight?: number;
+		/**
+		 * Maximum animation duration
+		 */
+		maxDuration: number;
+		/**
+		 * Minimum animation duration (defaults to 0)
+		 */
+		minDuration?: number;
+		/**
+		 * Maximum animation delay
+		 */
+		maxDelay: number;
+		/**
+		 * Minimum animation delay (defaults to 0)
+		 */
+		minDelay?: number;
+		/**
+		 * Y position to draw a rectangle of 0 height at
+		 */
+		baseY?: number;
+		/**
+		 * Whether to grow the rectangle from baseY in the positive or negative direction based on the
+		 * generated height
+		 */
+		growDirection: 'positive' | 'negative';
+		/**
+		 * Easing function to apply to the animation
+		 */
+		easing?: ((t: number) => number) | undefined;
+	};
+	export let maxHeight;
+	export let minHeight = 0; // Default to 0
+	export let maxDuration;
+	export let minDuration = 0; // Default to 0
+	export let maxDelay;
+	export let minDelay = 0; // Default to 0
+	export let baseY = 0; // Default to 0
+	export let growDirection;
+	export let easing: $$Props['easing'] = undefined; // This needs to be allowed to be undefined
+
 	/**
-	 * The maximum height to draw the wave at
+	 * Helper function to generate a random value in the provided range
+	 * @param max the max of the range
+	 * @param min the min of the range
 	 */
-	export let maxHeight: number;
-	/**
-	 * The width to draw the rectangle at
-	 */
-	export let width: number;
-	/**
-	 * The x-position to draw the wave at
-	 */
-	export let x: number;
-	/**
-	 * The duration to apply to the animation
-	 */
-	export let duration: number;
-	/**
-	 * The delay to apply to the animation
-	 */
-	export let delay: number | undefined = undefined;
-	/**
-	 * Fill to apply to the drawn rectangle
-	 */
-	export let fill: string | undefined = undefined;
+	function generateRandomValueInRange(max: number, min: number) {
+		return Math.random() * (max - min) + min;
+	}
 
 	// Tween for the current height
-	const currentHeight = tweened<number>(undefined, {
-		duration,
-		delay,
-		easing: cubicInOut
+	const currentHeight = tweened<number>(minHeight, {
+		duration: generateRandomValueInRange(maxDuration, minDuration),
+		delay: generateRandomValueInRange(maxDelay, minDelay),
+		easing
 	});
 
 	onMount(() => {
 		// Updates the height of the rectangle periodically, forever
 		let frame = requestAnimationFrame(async function updateHeight() {
-			await currentHeight.set($currentHeight == 0 ? Math.random() * maxHeight : 0);
+			await currentHeight.set(
+				$currentHeight == minHeight
+					? generateRandomValueInRange(maxHeight, minHeight)
+					: (minHeight ?? 0),
+				{
+					duration: generateRandomValueInRange(maxDuration, minDuration),
+					delay: generateRandomValueInRange(maxDelay, minDelay)
+				}
+			);
 			frame = requestAnimationFrame(updateHeight);
 		});
 
@@ -48,4 +89,8 @@
 	});
 </script>
 
-<rect {fill} height={$currentHeight} {x} {width} y={maxHeight - $currentHeight} />
+<rect
+	height={$currentHeight}
+	{...$$restProps}
+	y={(baseY ?? 0) + (growDirection == 'positive' ? 0 : -1) * $currentHeight}
+/>
--- src/lib/components/hero-section/waves.svelte
+++ src/lib/components/hero-section/waves.svelte
@@ -1,11 +1,12 @@
 <script lang="ts">
 	import Wave from './wave.svelte';
 
-	// Dimensions for the bar
+	// Dimensions for the bar and animation
 	const barSpacing = 5;
 	const barWidth = 20;
-	const duration = 500;
-	const delayStagger = 50;
+	const minDuration = 250;
+	const maxDuration = 500;
+	const maxDelay = 50;
 	const totalBarWidth = barSpacing + barWidth;
 
 	// Dimensions of the container box, used for calculation
@@ -23,19 +24,31 @@
 	bind:clientHeight={svgContainerHeight}
 >
 	<svg class="h-full w-full">
-		<linearGradient x1="0" x2="0" y1="1" y2="0" id="WaveGradient">
+		<linearGradient
+			x1="0"
+			x2="0"
+			y1={isNaN(svgContainerHeight) ? 0 : `${svgContainerHeight}px`}
+			y2="0"
+			gradientUnits="userSpaceOnUse"
+			id="WaveGradient"
+		>
 			<stop offset="0%" stop-color="red" />
 			<stop offset="50%" stop-color="yellow" />
 			<stop offset="100%" stop-color="green" />
 		</linearGradient>
 		{#each bars as _, barIndex}
 			<Wave
+				stroke="hsl(var(--primary))"
 				fill="url(#WaveGradient)"
+				growDirection="negative"
+				minHeight={svgContainerHeight / 8}
 				maxHeight={svgContainerHeight}
+				baseY={svgContainerHeight}
 				width={barWidth}
 				x={barIndex * totalBarWidth}
-				{duration}
-				delay={delayStagger * barIndex}
+				{minDuration}
+				{maxDuration}
+				{maxDelay}
 			/>
 		{/each}
 	</svg>
