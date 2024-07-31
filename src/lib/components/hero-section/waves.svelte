865207e484f379e72c01f7784943a1c435936e71
Ian W <49083526+Landaman@users.noreply.github.com>
2024-07-31 18:58:56 -0500

    Use a realistic looking hero animation
    Switches from completely random animation data to use
    simplex-noise (a NPM package based on the well-known
    algorithm by Ken Perlin). This creates much
    more realistic looking sound (at a higher computational
    cost)

--- src/lib/components/hero-section/waves.svelte
+++ src/lib/components/hero-section/waves.svelte
@@ -1,21 +1,62 @@
 <script lang="ts">
-	import Wave from './wave.svelte';
+	import { tweened } from 'svelte/motion';
+	import { createNoise2D } from 'simplex-noise';
+	import { onMount } from 'svelte';
 
 	// Dimensions for the bar and animation
 	const barSpacing = 5;
-	const barWidth = 20;
-	const minDuration = 250;
-	const maxDuration = 500;
-	const maxDelay = 50;
+	const barWidth = 10;
 	const totalBarWidth = barSpacing + barWidth;
+	const noiseScale = 0.05; // Scale of the noise (lower values = smoother noise)
+	const durationMs = 100; // Duration of each animation frame. Lower is better (but more CPU)
+	const millisecondsInSecond = 1000;
+	const strokeWidth = 1; // Stroke width in px
 
 	// Dimensions of the container box, used for calculation
 	let svgContainerWidth: number;
 	let svgContainerHeight: number;
 
-	// Generate the bars. This may crop the top bar but that's fine
+	// Calculate dimensions
+	$: minimumBarHeight = svgContainerHeight / 8;
+	$: maximumBarHeight = svgContainerHeight;
+
+	const noise = createNoise2D(); // This will generate realistic looking noise
+	let time = 0; // Time, so the wave varies
+
+	// Generate the bars. This may crop the last bar but that's fine
 	$: numBars = Math.floor(svgContainerWidth / totalBarWidth);
-	$: bars = new Array<number>(isNaN(numBars) ? 0 : numBars);
+
+	// Using undefined as initial makes it immediately animate to the
+	// first value (when we have no bars yet). Using NaN makes it so we
+	// immediately animate to the first values
+	$: bars = tweened<number[]>(isNaN(numBars) ? undefined : new Array(numBars).fill(NaN), {
+		duration: durationMs
+	});
+
+	// Start running on component mount
+	onMount(() => {
+		// Animate
+		let frame = requestAnimationFrame(async function updateBars() {
+			// Await makes it wait until the animation finishes (duration)
+			await bars.set(
+				$bars.map(
+					(_, index) =>
+						((noise(
+							time / millisecondsInSecond, // Time should be in seconds
+							index * noiseScale // This converts to the position with scale
+						) +
+							1) /
+							2) * // noise is [-1, 1], so this converts [0, 1]
+							(maximumBarHeight - minimumBarHeight) +
+						minimumBarHeight // This applies height
+				)
+			);
+			time += durationMs; // Time is in seconds
+			frame = requestAnimationFrame(updateBars); // Update again
+		});
+
+		return () => cancelAnimationFrame(frame); // Cleanup the animation
+	});
 </script>
 
 <div
@@ -24,32 +65,34 @@
 	bind:clientHeight={svgContainerHeight}
 >
 	<svg class="h-full w-full">
-		<linearGradient
-			x1="0"
-			x2="0"
-			y1={isNaN(svgContainerHeight) ? 0 : `${svgContainerHeight}px`}
-			y2="0"
-			gradientUnits="userSpaceOnUse"
-			id="WaveGradient"
-		>
-			<stop offset="0%" stop-color="red" />
-			<stop offset="50%" stop-color="yellow" />
-			<stop offset="100%" stop-color="green" />
-		</linearGradient>
-		{#each bars as _, barIndex}
-			<Wave
-				stroke="hsl(var(--primary))"
-				fill="url(#WaveGradient)"
-				growDirection="negative"
-				minHeight={svgContainerHeight / 8}
-				maxHeight={svgContainerHeight}
-				baseY={svgContainerHeight}
-				width={barWidth}
-				x={barIndex * totalBarWidth}
-				{minDuration}
-				{maxDuration}
-				{maxDelay}
-			/>
-		{/each}
+		{#if minimumBarHeight && maximumBarHeight}
+			<linearGradient
+				x1="0"
+				x2="0"
+				y1={`${maximumBarHeight}px`}
+				y2={`${minimumBarHeight}px`}
+				gradientUnits="userSpaceOnUse"
+				id="WaveGradient"
+			>
+				<stop offset="0%" stop-color="red" />
+				<stop offset="50%" stop-color="yellow" />
+				<stop offset="100%" stop-color="green" />
+			</linearGradient>
+			{#if $bars}
+				{#each $bars as height, barIndex}
+					{#if height}
+						<rect
+							{height}
+							stroke-width={strokeWidth}
+							stroke="hsl(var(--primary))"
+							fill="url(#WaveGradient)"
+							y={maximumBarHeight - height}
+							width={barWidth}
+							x={strokeWidth + barIndex * totalBarWidth}
+						/>
+					{/if}
+				{/each}
+			{/if}
+		{/if}
 	</svg>
 </div>
