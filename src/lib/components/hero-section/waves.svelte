09ebbcbb8308d4143754d082cf864858c3bd427e
Ian W <49083526+Landaman@users.noreply.github.com>
2024-11-18 18:08:25 -0500

    Include a transition powered hero section
    This improves performance over the Tween hero section (and greatly
    reduces CPU load) because JS no longer has to power the transitions
    pixel-by-pixel for 100ms, instead the browser handles it natively
    and we just set the next wave points via JS

--- src/lib/components/hero-section/waves.svelte
+++ src/lib/components/hero-section/waves.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-	import { tweened } from 'svelte/motion';
 	import { createNoise2D } from 'simplex-noise';
 	import { onMount } from 'svelte';
 	import { fade } from 'svelte/transition';
@@ -24,48 +23,61 @@
 	const maximumBarHeight = $derived(svgContainerHeight);
 
 	const noise = createNoise2D(); // This will generate realistic looking noise
-	let time = 0; // Time, so the wave varies
+	const startTime = new Date().getTime(); // Start time of the animation
 
 	// Generate the bars. This may crop the last bar but that's fine
 	const numBars = $derived(Math.ceil(svgContainerWidth / totalBarWidth));
 
-	// Using undefined as initial makes it immediately animate to the
-	// first value (when we have no bars yet). Using NaN makes it so we
-	// immediately animate to the first values
-	const bars = $derived(
-		tweened<number[]>(isNaN(numBars) ? undefined : new Array(numBars).fill(NaN), {
-			duration: durationMs
-		})
-	);
+	// Bars, so it is intrinsically reactive
+	let bars = $state(new Array<number>(0).fill(0));
+	$effect(() => {
+		if (numBars == bars.length) {
+			return; // If we don't manually cancel this, we will loop forever updating bars since we read bars at some leve
+		}
+
+		bars = new Array<number>(numBars).fill(0); // Re-create bars
+		reAnimate(); // Make sure all the bars have a reasonable height
+	});
+
+	let animationFrame = 0; // Animation frame, so we can cancel on close
+	let nextAnimationTimer = 0; // Animation timer, so we can cancel on close
+	/**
+	 * Triggers an immediate re-animation of the bars, canceling any change in place and then re-scheduling the next change.
+	 * Accounts for the fact that a variable amount of time may have passed
+	 */
+	function reAnimate() {
+		cancelAnimationFrame(animationFrame); // In case we're already animating, cancel that because this will run on resize
+		clearTimeout(nextAnimationTimer); // Cancel the next timeout, as we will reset that because we will run on resize
+		// Update every bar
+		for (let index = 0; index < bars.length; index++) {
+			bars[index] =
+				((noise(
+					(new Date().getTime() - startTime) / millisecondsInSecond, // Time should be in seconds
+					index * noiseScale // This converts to the position with scale
+				) +
+					1) /
+					2) * // noise is [-1, 1], so this converts [0, 1]
+					(maximumBarHeight - minimumBarHeight) +
+				minimumBarHeight; // This applies height
+		}
+		nextAnimationTimer = window.setTimeout(
+			() => (animationFrame = requestAnimationFrame(reAnimate)),
+			durationMs
+		); // Update again. Call window to avoid weird bun issues
+	}
 
 	// Start running on component mount
 	onMount(() => {
 		mounted = true;
 
 		// Animate
-		let frame = requestAnimationFrame(async function updateBars() {
-			// Await makes it wait until the animation finishes (duration)
-			await bars.set(
-				$bars.map(
-					(_, index) =>
-						((noise(
-							time / millisecondsInSecond, // Time should be in seconds
-							index * noiseScale // This converts to the position with scale
-						) +
-							1) /
-							2) * // noise is [-1, 1], so this converts [0, 1]
-							(maximumBarHeight - minimumBarHeight) +
-						minimumBarHeight // This applies height
-				)
-			);
-			time += durationMs; // Time is in seconds
-			frame = requestAnimationFrame(updateBars); // Update again
-		});
+		animationFrame = requestAnimationFrame(reAnimate);
 
 		return () => {
 			mounted = false;
 
-			cancelAnimationFrame(frame);
+			cancelAnimationFrame(animationFrame);
+			clearTimeout(nextAnimationTimer);
 		}; // Cleanup the animation
 	});
 </script>
@@ -89,7 +101,7 @@
 				<stop offset="50%" stop-color="yellow" />
 				<stop offset="100%" stop-color="green" />
 			</linearGradient>
-			{#each $bars as height, barIndex}
+			{#each bars as height, barIndex}
 				<rect
 					{height}
 					stroke-width={strokeWidth}
@@ -98,6 +110,7 @@
 					y={maximumBarHeight - height}
 					width={barWidth}
 					x={strokeWidth + barIndex * totalBarWidth}
+					class="transition-[height,y] duration-100 ease-linear"
 				/>
 			{/each}
 		</svg>
