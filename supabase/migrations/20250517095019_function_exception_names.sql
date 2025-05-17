aa497c945546d031da186c64818a2040b0e313fa
Ian Wright <49083526+Landaman@users.noreply.github.com>
2025-05-17 06:12:32 -0400

    Use exception names for Supabase functions
    This uses a boxed "exception name" (i.e., HasPartnerException) as the
    message, with the real message in the "detail" field. This way, it is
    easier to string parse and determine what the error means on the client.

---
+++ supabase/migrations/20250517095019_function_exception_names.sql
@@ -0,0 +1,85 @@
+set check_function_bodies = off;
+
+CREATE OR REPLACE FUNCTION public.get_or_create_pairing_code()
+ RETURNS pairing_codes
+ LANGUAGE plpgsql
+ SET search_path TO 'public'
+AS $function$
+DECLARE
+  result pairing_codes;
+  done bool := false;
+BEGIN
+  IF (SELECT partner_id FROM profiles WHERE id = auth.uid()) IS NOT NULL
+  THEN
+    RAISE EXCEPTION 'HasPartnerException'
+      USING DETAIL = 'Unable to get a pairing code for a user that already has a partner';
+  END IF;
+
+  SELECT * INTO result
+  FROM pairing_codes
+  WHERE owner_id = auth.uid();
+
+  IF NOT FOUND OR result.expires_at < now() THEN
+    DELETE FROM pairing_codes WHERE owner_id = auth.uid();
+
+    WHILE NOT done LOOP
+      result.code := md5('' || now()::text || random()::text);
+      done := NOT EXISTS (
+        SELECT 1 FROM pairing_codes WHERE code = result.code
+      );
+    END LOOP;
+
+    INSERT INTO pairing_codes(owner_id, code, expires_at)
+    VALUES (auth.uid(), result.code, now() + interval '15 minutes')
+    RETURNING * INTO result;
+  END IF;
+
+  RETURN result;
+END;
+$function$
+;
+
+CREATE OR REPLACE FUNCTION public.pair_with_code(pairing_code character varying)
+ RETURNS void
+ LANGUAGE plpgsql
+ SET search_path TO 'public'
+AS $function$
+DECLARE
+  code_expiry TIMESTAMPTZ;
+  code_owner_id UUID;
+BEGIN  
+  IF (SELECT partner_id FROM profiles WHERE id = auth.uid()) IS NOT NULL 
+  THEN
+    RAISE EXCEPTION 'HasPartnerException'
+      USING DETAIL = 'Unable to pair with a code when the user already has a partner';
+  END IF;
+
+  SELECT owner_id, expires_at INTO code_owner_id, code_expiry FROM pairing_codes WHERE code = pairing_code;
+
+  IF code_owner_id = auth.uid()
+  THEN
+    RAISE EXCEPTION 'InvalidPairingCodeException'
+      USING DETAIL = 'Unable to pair with a code the user owns';
+  END IF;
+
+  IF NOT FOUND OR code_expiry < NOW()
+  THEN
+    RAISE EXCEPTION 'InvalidPairingCodeException'
+      USING DETAIL = 'Unable to pair with a pairing code that does not exist or is expired';
+  END IF;
+
+  IF (SELECT partner_id FROM profiles WHERE id = code_owner_id) IS NOT NULL
+  THEN
+    RAISE EXCEPTION 'InvalidPairingCodeException'
+      USING DETAIL = 'Unable to pair with a pairing code that does not exist or is expired';
+  END IF;
+
+  UPDATE profiles SET partner_id = code_owner_id WHERE id = auth.uid();
+  UPDATE profiles SET partner_id = auth.uid() WHERE id = code_owner_id;
+
+  DELETE FROM pairing_codes WHERE code = pairing_code;
+END;
+$function$
+;
+
+
--- supabase/schemas/pairing_codes.sql
+++ supabase/schemas/pairing_codes.sql
@@ -25,24 +25,28 @@ DECLARE
 BEGIN  
   IF (SELECT partner_id FROM profiles WHERE id = auth.uid()) IS NOT NULL 
   THEN
-    RAISE EXCEPTION 'Unable to pair with a code when the user already has a partner';
+    RAISE EXCEPTION 'HasPartnerException'
+      USING DETAIL = 'Unable to pair with a code when the user already has a partner';
   END IF;
 
   SELECT owner_id, expires_at INTO code_owner_id, code_expiry FROM pairing_codes WHERE code = pairing_code;
 
   IF code_owner_id = auth.uid()
   THEN
-    RAISE EXCEPTION 'Unable to pair with a code the user owns';
+    RAISE EXCEPTION 'InvalidPairingCodeException'
+      USING DETAIL = 'Unable to pair with a code the user owns';
   END IF;
 
-  IF code_expiry IS NULL OR code_expiry < NOW()
+  IF NOT FOUND OR code_expiry < NOW()
   THEN
-    RAISE EXCEPTION 'Invalid pairing code';
+    RAISE EXCEPTION 'InvalidPairingCodeException'
+      USING DETAIL = 'Unable to pair with a pairing code that does not exist or is expired';
   END IF;
 
   IF (SELECT partner_id FROM profiles WHERE id = code_owner_id) IS NOT NULL
   THEN
-    RAISE EXCEPTION 'Unable to pair with a code from a user that has a partner';
+    RAISE EXCEPTION 'InvalidPairingCodeException'
+      USING DETAIL = 'Unable to pair with a pairing code that does not exist or is expired';
   END IF;
 
   UPDATE profiles SET partner_id = code_owner_id WHERE id = auth.uid();
@@ -62,7 +66,8 @@ DECLARE
 BEGIN
   IF (SELECT partner_id FROM profiles WHERE id = auth.uid()) IS NOT NULL
   THEN
-    RAISE EXCEPTION 'Unable to get a pairing code for a user that already has a partner';
+    RAISE EXCEPTION 'HasPartnerException'
+      USING DETAIL = 'Unable to get a pairing code for a user that already has a partner';
   END IF;
 
   SELECT * INTO result
@@ -87,3 +92,4 @@ BEGIN
   RETURN result;
 END;
 $$;
+
