1d2f5dc9397d881dcd65716e439134fddb5b31b4
Ian Wright <49083526+Landaman@users.noreply.github.com>
2025-07-14 10:56:56 -0400

    Add a function to get the client credentials token
    Some calls (e.g., getting the detail of a song) should be preformed on
    behalf of the app, and not the user. This function enables you to get an
    access token to be able to do that

--- supabase/schemas/spotify.sql
+++ supabase/schemas/spotify.sql
@@ -28,3 +28,27 @@ RETURN extensions.http_header ('Authorization', 'Basic ' || translate(
   ));
 END;
 $$;
+
+CREATE FUNCTION private.get_client_credentials_access_token () RETURNS character varying LANGUAGE plpgsql
+SET
+  search_path = '' AS $$ DECLARE
+    return_status int;
+    access_token character varying;
+BEGIN
+    SELECT
+        "status",
+        "content"::jsonb ->> 'access_token' INTO return_status,
+        access_token
+    FROM
+        extensions.http (('POST', 'https://accounts.spotify.com/api/token?grant_type=client_credentials', array[
+        private.get_basic_credentials_header (),
+        extensions.http_header ('Accept', 'application/json')
+      ],
+      'application/x-www-form-urlencoded',
+      '')::extensions.http_request);
+    IF return_status != 200 THEN
+        RAISE EXCEPTION 'InvalidClientCredentials' USING DETAIL = 'The provided client credentials are invalid or missing';
+    END IF;
+    RETURN access_token;
+END;
+$$;
